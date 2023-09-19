01766e80118379c15b9eb433a2d860d25727fd24
sonnt210 <sonnt.code@gmail.com>
2023-09-19 22:38:31 +0700

    add file

---
+++ database/migrations/2023_09_17_143440_create_stories_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stories', function (Blueprint $table) {
+            $table->id();
+            $table->string('story_name')->unique()->nullable(false);
+            $table->string('story_slug')->unique()->nullable(false);
+            $table->text('summary')->nullable(true);
+            $table->integer('genre_id')->nullable(true);
+            $table->string('image')->nullable(true);
+            $table->tinyInteger('active');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stories');
+    }
+}
